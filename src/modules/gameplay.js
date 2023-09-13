e43b38ea8c5815fb9e55767e9f213208fe8c105b
Николай Чистяков <cats@feel.so>
2023-09-13 14:04:25 +0300

    [+] generate game field

--- src/modules/gameplay.js
+++ src/modules/gameplay.js
@@ -1,13 +1,22 @@
 // in MVC term, this module represents the game's Model
+import { irand } from "math";
 const N = 9;
 const M = 10;
+const len = N * M;
 const C = 5;
 
 let field;
 
 const setup = () => {
   console.log(`setting up gameplay for an ${N} by ${M} field`);
-  field = new UInt8Array(N * N);
+  field = new Uint8Array(len);
+
+  // generate game field
+  for (let i = 0; i < len; i++) {
+    // items are represented by numbers 1..C
+    // with 0 signifying empty space
+    field[i] = irand(C) + 1;
+  }
 };
 
 const teardown = () => {};
