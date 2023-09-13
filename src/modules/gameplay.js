fe96e5a39433139f102d1616814a43b13671e2d3
Николай Чистяков <cats@feel.so>
2023-09-13 22:06:30 +0300

    [+] add 2d array manipulation helpers

--- src/modules/gameplay.js
+++ src/modules/gameplay.js
@@ -1,11 +1,23 @@
 // in MVC term, this module represents the game's Model
-import { irand } from "math";
-const N = 9;
-const M = 10;
+import { irand, clamp } from "math";
+const N = 2;
+const M = 3;
 const len = N * M;
 const C = 5;
 
 let field;
+
+// get value at (x, y) in field f
+const get = (f, x, y) => f[clamp(y, 0, M - 1) * N + clamp(x, 0, N - 1)];
+
+// set value at (x, y) in field f
+const set = (f, x, y, v) => {
+  f[clamp(y, 0, M - 1) * N + clamp(x, 0, N - 1)] = v;
+};
+
+// expose get and set to the console
+window.get = (x, y) => get(field, x, y);
+window.set = (x, y, v) => set(field, x, y, v);
 
 const printNicely = () => {
   const s = field.reduce((acc, v, i) => acc + ((i + 1) % N ? v : `${v}\n`), "");
