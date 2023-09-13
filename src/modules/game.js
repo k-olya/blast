a8d8337812f1b08a4101ddfb9fcb78e5b04075b3
Николай Чистяков <cats@feel.so>
2023-09-13 13:55:03 +0300

    [+] add onload handler to the game module

--- src/modules/game.js
+++ src/modules/game.js
@@ -1,11 +1,27 @@
+// this is the module manager
+// it tells other modules when to start and when to stop
 import { Loading } from "modules/loading";
+import { Gameplay } from "modules/gameplay";
+import { on } from "ev";
 
 const currentScene = "loading";
+
+// decides what to do when loading is finished
+const onload = () => {
+  Loading.teardown();
+  console.log("game loaded");
+  Gameplay.setup();
+};
 
 const setup = () => {
   console.log("initializing game...");
   Loading.setup();
+
+  // register event handlers
+  on("load", onload);
 };
 
+const teardown = () => {};
+
 export const SCENE_ENUM = ["loading", "playing", "gameover"];
-export const Game = { setup };
+export const Game = { setup, teardown };
