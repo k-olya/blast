b73812277c006d815ecc8d8ea5d614e2cf1f35f8
Николай Чистяков <cats@feel.so>
2023-09-18 15:30:08 +0300

    [+] add a function to find groups

--- src/modules/gameplay.js
+++ src/modules/gameplay.js
@@ -1,11 +1,13 @@
 // in MVC term, this module represents the game's Model
 import { irand, clamp } from "math";
-const N = 2;
-const M = 3;
+const N = 9;
+const M = 10;
 const len = N * M;
 const C = 5;
+const K = 2;
 
 let field;
+let Kgroup;
 
 // get value at (x, y) in field f
 const get = (f, x, y) => f[clamp(y, 0, M - 1) * N + clamp(x, 0, N - 1)];
@@ -19,9 +21,64 @@ const set = (f, x, y, v) => {
 window.get = (x, y) => get(field, x, y);
 window.set = (x, y, v) => set(field, x, y, v);
 
-const printNicely = () => {
-  const s = field.reduce((acc, v, i) => acc + ((i + 1) % N ? v : `${v}\n`), "");
+// debug output for the playing field
+const printNicely = f => {
+  const s = f.reduce((acc, v, i) => acc + ((i + 1) % N ? v : `${v}\n`), "");
   console.log(s);
+};
+
+// map this to get all cells adjacent to a given cell
+const adjacents = [
+  [1, 0],
+  [0, 1],
+  [-1, 0],
+  [0, -1],
+];
+
+// find a group of items and calculate its size
+const findKGroup = (x, y) => {
+  Kgroup = new Uint8Array(len);
+  // v is the value of the item under cursor
+  const v = get(field, x, y);
+  // k is a counter for adjacent items
+  let k = 1;
+  set(Kgroup, x, y, 1);
+  // place directly adjacent cells to the processing stack
+  // but only if they contain the necessary value
+  let stack = adjacents
+    .map(d => [x + d[0], y + d[1]])
+    .filter(a => get(field, a[0], a[1]) === v);
+  // use for from 0 to a very large number instead of while
+  for (let i = 0; i < 0x7fffffff && stack.length; i++) {
+    const [currentX, currentY] = stack.pop();
+    const currentValue = get(field, currentX, currentY);
+    if (get(Kgroup, currentX, currentY) === 0) {
+      // inc the counter
+      k++;
+      // mark cell as visited and belonging to the group
+      set(Kgroup, currentX, currentY, 1);
+      for (let a of adjacents) {
+        // calculate coordinates of an adjacent cell
+        const s = [a[0] + currentX, a[1] + currentY];
+        // if it's our value and the cell in question hasn't been visited
+        if (v === get(field, s[0], s[1]) && get(Kgroup, s[0], s[1]) === 0) {
+          stack.push(s);
+        }
+      }
+    }
+  }
+  // if there are unprocessed cells left
+  // it means we've hit a bug
+  if (stack.length) {
+    console.error("infinite loop in findKGroup");
+  }
+
+  printNicely(Kgroup);
+  console.log(k);
+};
+
+window.makeTurn = (x, y) => {
+  findKGroup(x, y);
 };
 
 const setup = () => {
@@ -34,7 +91,7 @@ const setup = () => {
     // with 0 signifying empty space
     field[i] = irand(C) + 1;
   }
-  printNicely();
+  printNicely(field);
 };
 
 const teardown = () => {};
