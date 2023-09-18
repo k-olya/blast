ec9bc672e6ded051cbc592c6ec65e8d509db80c0
Николай Чистяков <cats@feel.so>
2023-09-19 00:25:18 +0300

    [+] add blast logic

--- src/modules/gameplay.js
+++ src/modules/gameplay.js
@@ -1,5 +1,7 @@
 // in MVC term, this module represents the game's Model
 import { irand, clamp } from "math";
+import { emit } from "ev";
+
 const N = 9;
 const M = 10;
 const len = N * M;
@@ -7,7 +9,7 @@ const C = 5;
 const K = 2;
 
 let field;
-let Kgroup;
+let group;
 
 // get value at (x, y) in field f
 const get = (f, x, y) => f[clamp(y, 0, M - 1) * N + clamp(x, 0, N - 1)];
@@ -37,12 +39,12 @@ const adjacents = [
 
 // find a group of items and calculate its size
 const findKGroup = (x, y) => {
-  Kgroup = new Uint8Array(len);
+  group = new Uint8Array(len);
   // v is the value of the item under cursor
   const v = get(field, x, y);
   // k is a counter for adjacent items
   let k = 1;
-  set(Kgroup, x, y, 1);
+  set(group, x, y, 1);
 
   // js engines don't have tail-call optimizations
   // so we're going to have to use a stack-based algorithm
@@ -55,16 +57,16 @@ const findKGroup = (x, y) => {
   // use for from 0 to a very large number instead of while
   for (let i = 0; i < 0x7fffffff && stack.length; i++) {
     const [currentX, currentY] = stack.pop();
-    if (get(Kgroup, currentX, currentY) === 0) {
+    if (get(group, currentX, currentY) === 0) {
       // inc the counter
       k++;
       // mark cell as visited and belonging to the group
-      set(Kgroup, currentX, currentY, 1);
+      set(group, currentX, currentY, 1);
       for (let a of adjacents) {
         // calculate coordinates of an adjacent cell
         const s = [a[0] + currentX, a[1] + currentY];
         // if it's our value and the cell in question hasn't been visited
-        if (v === get(field, s[0], s[1]) && get(Kgroup, s[0], s[1]) === 0) {
+        if (v === get(field, s[0], s[1]) && get(group, s[0], s[1]) === 0) {
           stack.push(s);
         }
       }
@@ -76,12 +78,89 @@ const findKGroup = (x, y) => {
     console.error("infinite loop in findKGroup");
   }
 
-  printNicely(Kgroup);
-  console.log(k);
+  return { group, k };
+};
+
+// generate falling pillars of items
+const getPillars = group => {
+  const pillars = [];
+  for (let i = 0; i < N; i++) {
+    // go from top to bottom
+    let fallHeight = 0;
+    let currentPillar = [];
+    for (let j = M - 1; j >= 0; j--) {
+      // if it's a chunk of empty space
+      // push current pillar to the array
+      if (get(group, i, j) === 1) {
+        fallHeight++;
+        if (currentPillar.length) {
+          pillars.push({
+            x: i,
+            y: j + 1,
+            height: fallHeight - 1,
+            content: currentPillar,
+          });
+          currentPillar = [];
+        }
+        // else add elements to the current pillar
+      } else if (fallHeight) {
+        currentPillar.push(get(field, i, j));
+      }
+    }
+    // add remaining pillar to the array
+    // and fill empty space with new items
+    if (fallHeight) {
+      for (let j = 0; j < fallHeight; j++) {
+        currentPillar.push(irand(C) + 1);
+      }
+      pillars.push({
+        x: i,
+        y: -fallHeight,
+        height: fallHeight,
+        content: currentPillar,
+      });
+    }
+  }
+  return pillars;
+};
+
+// remove blasted and moving cells
+const removeCells = group => {
+  for (let i = 0; i < N; i++) {
+    // go from top to bottom
+    let fallHeight = 0;
+    for (let j = M - 1; j >= 0; j--) {
+      if (get(group, i, j) === 1) {
+        fallHeight++;
+      }
+      if (fallHeight) {
+        set(field, i, j, 0);
+      }
+    }
+  }
 };
 
 window.makeTurn = (x, y) => {
-  findKGroup(x, y);
+  const { group, k } = findKGroup(x, y);
+  printNicely(group);
+  console.log("k", k);
+  // schedule animations
+  if (k < K) {
+    emit("bad-move", group);
+    return;
+  }
+  emit("blast", group);
+
+  // get falling pillars
+  const pillars = getPillars(group);
+  // clear removed and falling items
+  removeCells(group);
+  printNicely(field);
+  // push intermediate state to the renderer
+  emit("field", field);
+  // tell renderer to queue fall animation
+  emit("falling", pillars);
+  console.log(pillars);
 };
 
 const setup = () => {
@@ -94,6 +173,9 @@ const setup = () => {
     // with 0 signifying empty space
     field[i] = irand(C) + 1;
   }
+  // emit field update event
+  // for the renderer to handle
+  emit("field", field);
   printNicely(field);
 };
 
